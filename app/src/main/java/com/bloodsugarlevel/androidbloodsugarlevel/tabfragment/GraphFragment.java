cd12c237087367d124f1fd2eb5c6582f8ecd8965
demiurgZorge <andrey777zhuravl@gmail.com>
2018-12-19 11:18:09 +0300

    add ApiResult fix

--- app/src/main/java/com/bloodsugarlevel/androidbloodsugarlevel/tabfragment/GraphFragment.java
+++ app/src/main/java/com/bloodsugarlevel/androidbloodsugarlevel/tabfragment/GraphFragment.java
@@ -15,19 +15,24 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.bloodsugarlevel.androidbloodsugarlevel.R;
 import com.bloodsugarlevel.androidbloodsugarlevel.dto.SugarDto;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.bloodsugarlevel.common.ApiListResult;
+import com.bloodsugarlevel.common.FilterState;
+import com.bloodsugarlevel.common.PagingState;
+import com.bloodsugarlevel.common.QueryState;
+import com.bloodsugarlevel.common.SortState;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 import org.json.JSONArray;
-import org.json.JSONException;
+import org.json.JSONObject;
 
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class GraphFragment extends Fragment implements Button.OnClickListener{
@@ -62,15 +67,15 @@ public class GraphFragment extends Fragment implements Button.OnClickListener{
         return mFragmentView;
     }
 
-    private void setGraphWithSugarLevelData(GraphView graph, ArrayList<SugarDto> listDto) {
+    private void setGraphWithSugarLevelData(GraphView graph, List<SugarDto> listDto) {
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
         for(SugarDto dto : listDto){
             series.appendData(new DataPoint(dto.date, dto.level), true, listDto.size());
         }
         graph.addSeries(series);
 
-//        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getActivity()));
-//        graph.getGridLabelRenderer().setNumHorizontalLabels( listDto.size()-1);
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getActivity()));
+        graph.getGridLabelRenderer().setNumHorizontalLabels( listDto.size()-1);
 //
 ////        // set manual x bounds to have nice steps
 //        graph.getViewport().setMinX(listDto.get(0).date.getTime());
@@ -87,54 +92,115 @@ public class GraphFragment extends Fragment implements Button.OnClickListener{
 
     private void getRange(final GraphView graph) {
         mRequestQueue = Volley.newRequestQueue(getContext());
-        String url ="http://78.46.233.90:8080/auth/sugar/getrange";
+        String url ="http://78.46.233.90:8080/auth/sugar/getrange/1";
+        List<FilterState> filters = new ArrayList<>();
+        PagingState pagingState = new PagingState();
+        SortState sortState = new SortState();
+        QueryState query = new QueryState(pagingState, sortState, filters);
 
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        ArrayList<SugarDto> listDto = extractSugarDtoList(response);
-                        setGraphWithSugarLevelData(graph, listDto);
-                    }
-                }, new Response.ErrorListener() {
+//        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.POST, url, query.toJSONArray(),
+//                new Response.Listener<JSONArray>() {
+//                    @Override
+//                    public void onResponse(JSONArray response) {
+//                        List<SugarDto> listDto = extractSugarDtoList(response);
+//                        setGraphWithSugarLevelData(graph, listDto);
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                String string = error.toString();
+//                showAlertDialog(string);
+//            }
+//        });
+//        jsonArrayRequest.setTag(HTTP_VOLEY_TAG);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, query.toJSONObject(), new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                List<SugarDto> listDto = extractSugarDtoList(response);
+                setGraphWithSugarLevelData(graph, listDto);
+            }
+        }, new Response.ErrorListener() {
+
             @Override
             public void onErrorResponse(VolleyError error) {
                 String string = error.toString();
-                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                    }
-                });
-                builder.setTitle("Error");
-                builder.setMessage(string);
-                AlertDialog dialog = builder.create();
-                dialog.show();
+                showAlertDialog(string);
+
             }
         });
-        jsonArrayRequest.setTag(HTTP_VOLEY_TAG);
+        jsonObjectRequest.setTag(HTTP_VOLEY_TAG);
 
-        mRequestQueue.add(jsonArrayRequest);
+        mRequestQueue.add(jsonObjectRequest);
     }
 
-    private ArrayList<SugarDto> extractSugarDtoList(JSONArray response) {
-        ObjectMapper mapper = new ObjectMapper();
+    private void showAlertDialog(String string) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+        builder.setTitle("Error");
+        builder.setMessage(string);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private List<SugarDto> extractSugarDtoList(JSONArray response) {
+
+        com.bloodsugarlevel.common.Response resp = new com.bloodsugarlevel.common.Response(response);
+        try {
+            ApiListResult<List<SugarDto>> listResult = ApiListResult.fromResponse(resp, SugarDto.class).getListResult();
+            return listResult.getData();
+        } catch (Exception e) {
+            showAlertDialog(e.getMessage());
+        }
 
         ArrayList<SugarDto> listDto = new ArrayList<>();
-        for (int i = 0; i < response.length() ; i++){
-            try {
-                Object obj = response.get(i);
-                SugarDto dto = null;
-                try {
-                    dto = mapper.readValue(obj.toString(), SugarDto.class);
-                    listDto.add(dto);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+//        for (int i = 0; i < response.length() ; i++){
+//            try {
+//                Object obj = response.get(i);
+//                SugarDto dto = null;
+//                try {
+//                    dto = mapper.readValue(obj.toString(), SugarDto.class);
+//                    listDto.add(dto);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//        }
+        return null;
+    }
+
+    private List<SugarDto> extractSugarDtoList(JSONObject response) {
+
+        com.bloodsugarlevel.common.Response resp = new com.bloodsugarlevel.common.Response(response);
+        try {
+            ApiListResult<List<SugarDto>> listResult = ApiListResult.fromResponse(resp, SugarDto.class).getListResult();
+            return listResult.getData();
+        } catch (Exception e) {
+            showAlertDialog(e.getMessage());
         }
-        return listDto;
+
+        ArrayList<SugarDto> listDto = new ArrayList<>();
+//        for (int i = 0; i < response.length() ; i++){
+//            try {
+//                Object obj = response.get(i);
+//                SugarDto dto = null;
+//                try {
+//                    dto = mapper.readValue(obj.toString(), SugarDto.class);
+//                    listDto.add(dto);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//        }
+        return null;
     }
 
     @Override
--- app/src/main/java/com/bloodsugarlevel/common/Response.java
+++ app/src/main/java/com/bloodsugarlevel/common/Response.java
@@ -1,13 +1,77 @@
 package com.bloodsugarlevel.common;
 
-public class Response {
-    private boolean statusCode;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-    public String jsonPath(){
+import java.util.Map;
+
+public class Response {
+    String string = null;
+    JSONArray jsonArray = null;
+    JSONObject jsonObject = null;
+    private boolean statusCode = false;
+
+    public Response() {
+
+    }
+
+    public Response(Object r) {
+        this();
+        if (r instanceof JSONArray) {
+            jsonArray = (JSONArray) r;
+            string = jsonArray.toString();
+        }
+        if (r instanceof JSONObject) {
+            jsonObject = (JSONObject) r;
+            string = jsonObject.toString();
+        }
+        if (r instanceof String) {
+            string = (String) r;
+        }
+    }
+
+    public Response jsonPath() {
+        return null;
+    }
+
+    public Map<String, Object> get() {
         return null;
     }
 
     public boolean getStatusCode() {
         return statusCode;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public JSONArray getJsonArray() {
+        return jsonArray;
+    }
+
+    public void setJsonArray(JSONArray jsonArray) {
+        this.jsonArray = jsonArray;
+    }
+
+    public JSONObject getJsonObject() {
+        return jsonObject;
+    }
+
+    public void setJsonObject(JSONObject jsonObject) {
+        this.jsonObject = jsonObject;
+    }
+
+    public void setStatusCode(boolean statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    @Override
+    public String toString() {
+        return string;
     }
 }
